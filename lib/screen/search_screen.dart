389a9a837a04386a4d0bf5705df24c7e32f062d6
rd <rohandas.zero@gmail.com>
2025-01-24 15:51:11 +0530

    search page

---
+++ lib/screen/search_screen.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+
+class BackgroundWave extends StatelessWidget {
+  final double height;
+
+  const BackgroundWave({super.key, required this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: ClipPath(
+          clipper: BackgroundWaveClipper(),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: height,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              colors: [Color(0xFFFACCCC), Color(0xFFF6EFE9)],
+            )),
+          )),
+    );
+  }
+}
+
+class BackgroundWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+
+    const minSize = 140.0;
+
+    // when h = max = 280
+    // h = 280, p1 = 210, p1Diff = 70
+    // when h = min = 140
+    // h = 140, p1 = 140, p1Diff = 0
+    final p1Diff = ((minSize - size.height) * 0.5).truncate().abs();
+    path.lineTo(0.0, size.height - p1Diff);
+
+    final controlPoint = Offset(size.width * 0.4, size.height);
+    final endPoint = Offset(size.width, minSize);
+
+    path.quadraticBezierTo(
+        controlPoint.dx, controlPoint.dy, endPoint.dx, endPoint.dy);
+
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(BackgroundWaveClipper oldClipper) => oldClipper != this;
+}
+
+
+
+
+class SearchBar extends StatelessWidget {
+  final pink = const Color(0xFFFACCCC);
+  final grey = const Color(0xFFF2F2F7);
+
+  const SearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width - 32,
+      child: TextFormField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          focusColor: pink,
+          focusedBorder: _border(pink),
+          border: _border(grey),
+          enabledBorder: _border(grey),
+          hintText: 'Start brand search',
+          contentPadding: const EdgeInsets.symmetric(vertical: 20),
+          prefixIcon: const Icon(
+            Icons.search,
+            color: Colors.grey,
+          ),
+        ),
+        onFieldSubmitted: (value) {},
+      ),
+    );
+  }
+
+  OutlineInputBorder _border(Color color) => OutlineInputBorder(
+    borderSide: BorderSide(width: 0.5, color: color),
+    borderRadius: BorderRadius.circular(12),
+  );
+}
+
+
+
+
+
+
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverPersistentHeader(
+            delegate: SliverSearchAppBar(),
+            pinned: true,
+          ),
+          SliverList(
+              delegate:
+                  SliverChildBuilderDelegate((BuildContext context, int index) {
+            return Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(16),
+                child: const Text(
+                  'Cruelty-free brand',
+                  style: TextStyle(fontSize: 20),
+                ));
+          }, childCount: 20))
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+class SliverSearchAppBar extends SliverPersistentHeaderDelegate {
+  const SliverSearchAppBar();
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    var adjustedShrinkOffset =
+    shrinkOffset > minExtent ? minExtent : shrinkOffset;
+    double offset = (minExtent - adjustedShrinkOffset) * 0.5;
+    double topPadding = MediaQuery.of(context).padding.top + 16;
+
+    return Stack(
+      children: [
+        const BackgroundWave(
+          height: 280,
+        ),
+        Positioned(
+          top: topPadding + offset,
+          left: 16,
+          right: 16,
+          child: const SearchBar(),
+        )
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => 280;
+
+  @override
+  double get minExtent => 140;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      oldDelegate.maxExtent != maxExtent || oldDelegate.minExtent != minExtent;
+}
